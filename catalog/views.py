e569fcf4e025dfd28c8d5367e3e6d4ae600a4399
Eisiehame <knurov24@gmail.com>
2023-05-05 23:43:15 +0200

    post

--- catalog/views.py
+++ catalog/views.py
@@ -5,4 +5,10 @@ def take_homepage(request):
     return render(request, 'catalog/take_homepage.html')
 
 def take_contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f'пользватель {name} , телефон {phone}, говорит {message}')
+
     return render(request, 'catalog/take_contact.html')
