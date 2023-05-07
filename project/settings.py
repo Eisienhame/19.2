08f6fe7963f5b774994ee742e99e6404f30d532f
Eisiehame <knurov24@gmail.com>
2023-05-07 17:16:45 +0200

    bd settings

--- project/settings.py
+++ project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'lesson20',
+        'USER': 'postgres',
+        'PASSWORD': f'{os.getenv("bd_pass")}'
     }
 }
 
