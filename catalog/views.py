a753dbfc3f591db85f118ecf7ed50f002018f09a
Eisiehame <knurov24@gmail.com>
2023-05-11 23:08:27 +0200

    as

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from catalog.models import Product
 
 # Create your views here.
 def take_homepage(request):
@@ -12,3 +13,9 @@ def take_contact(request):
         print(f'пользватель {name} , телефон {phone}, говорит {message}')
 
     return render(request, 'catalog/take_contact.html')
+
+def product_card(request):
+    context = {
+        'product_card' : Product.objects.all()
+    }
+    return render(request, 'catalog/product_card.html', context)
