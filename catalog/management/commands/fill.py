3e9d6aedb6c64bee84f8361e7d38a1d09e4f6ce9
Eisiehame <knurov24@gmail.com>
2023-05-19 18:50:19 +0200

    fill

--- catalog/management/commands/fill.py
+++ catalog/management/commands/fill.py
@@ -1,5 +1,5 @@
 from django.core.management import BaseCommand
-from catalog.models import Category
+from catalog.models import Category, Products
 import psycopg2, os
 
 class Command(BaseCommand):
@@ -24,3 +24,14 @@ class Command(BaseCommand):
             category_objects.append(Category(**i))
 
         Category.objects.bulk_create(category_objects)
+
+        product_list = [
+            {"name": "mango", "description": "fruits", "preview_image": "products/freshmango.jpg", "category": "fruits", "price": 124,"date_of_creation": "2023-05-14T22:18:42Z","Last_modified_date": "2023-05-14T22:18:42Z"},
+            {"name": "banan", "description": "fruits", "preview_image": "products/01banan.jpg", "category": "fruits", "price": 32,"date_of_creation": "2023-05-14T23:18:42Z","Last_modified_date": "2023-05-14T23:18:42Z"},
+            {"name": "Яблоко", "description": "fruits", "preview_image": "products/03яблоко.jpg", "category": "fruits", "price": 14,"date_of_creation": "2023-05-14T24:18:42Z","Last_modified_date": "2023-05-14T24:18:42Z"}
+        ]
+        product_objects = []
+        for i in categories_list:
+            product_objects.append(Category(**i))
+
+        Products.objects.bulk_create(product_objects)
